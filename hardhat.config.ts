46896e85b66f34590cec5602d1754f0400435057
Dale <dellryuzi@gmail.com>
2024-05-05 10:41:24 +0700

    build: change into paris vm

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          evmVersion: "paris",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
