8ca76734752abd5ddf1043722964d6563bb80ef4
Dale <dellryuzi@gmail.com>
2024-05-20 20:20:52 +0700

    doc: update address

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+// require('@nomiclabs/hardhat-ethers');
+// require('@openzeppelin/hardhat-upgrades');
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
